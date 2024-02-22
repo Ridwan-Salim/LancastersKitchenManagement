fd6c2cb54c9c9666d0454688d0610c77f382fa45
Ridwan-Salim <123819533+Ridwan-Salim@users.noreply.github.com>
2024-02-22 23:17:20 +0000

    Delete TeamProject/src/interfaces/Record.java
    Replaced with new interface

--- TeamProject/src/interfaces/Record.java
+++
@@ -1,8 +0,0 @@
-package interfaces;
-
-public interface Record {
-    <R> void saveReceipt(R receipt); // saves a receipt to the Management database
-    <B> void saveBooking(B booking); // saves a booking to the Management database
-
-    // Unknown params for saving
-}
