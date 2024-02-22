5866d5bc130d943feb39a451b2de0daeb3ac0816
Ridwan-Salim <123819533+Ridwan-Salim@users.noreply.github.com>
2024-02-22 23:17:05 +0000

    Delete TeamProject/src/interfaces/Menu.java
    Replaced with new interface

--- TeamProject/src/interfaces/Menu.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-import java.time.LocalDate;
-
-public interface Menu {
-    void getMenuData(); // returns the raw menu data for a certain day
-    void getMenuPDF(); // returns a printable PDF of the menu that can be downloaded
-
-}
