e315ef0a147413fb150ab36205bf22a72be19505
artem-korniienko <123883457+artem-korniienko@users.noreply.github.com>
2024-02-24 13:28:37 +0000

    Update IMenu.java

--- TeamProject/src/interfaces/FoH/IMenu.java
+++ TeamProject/src/interfaces/FoH/IMenu.java
@@ -1,7 +1,6 @@
 package interfaces.FoH;
 
 public interface IMenu {
-    void getMenuData(int UNIXtimestamp_Date); // returns the menu for a certain day with descriptions etc, updated weekly
     void getMenuPDF(int UNIXtimestamp_Date); // returns a printable PDF of the menu, updated weekly
 
 }
