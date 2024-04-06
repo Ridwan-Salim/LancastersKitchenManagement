ac9afa17ac6d819ca53d79dc98606e2c95c280a4
madam6 <artem.korniienko@city.ac.uk>
2024-04-06 22:00:56 +0100

    New screens

--- software/src/scenes/GeneralStaffScene.java
+++ software/src/scenes/GeneralStaffScene.java
@@ -1,12 +1,17 @@
 package scenes;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -14,33 +19,70 @@ import java.io.IOException;
 
 public class GeneralStaffScene extends PersonalizableScene {
     public Scene createScene() {
-        VBox layout = new VBox(20);
-        layout.setAlignment(Pos.CENTER);
+        BorderPane layout = new BorderPane();
         layout.setPadding(new Insets(20));
 
         Label greetingLabel = new Label("Welcome General Staff " + employeeName + "!");
         greetingLabel.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
+        BorderPane.setAlignment(greetingLabel, Pos.TOP_LEFT);
+        BorderPane.setMargin(greetingLabel, new Insets(10, 0, 0, 10));
+
+        Timeline labelAnimation = new Timeline(
+                new KeyFrame(Duration.ZERO, new KeyValue(greetingLabel.opacityProperty(), 0)),
+                new KeyFrame(Duration.seconds(1), new KeyValue(greetingLabel.opacityProperty(), 1))
+        );
+        labelAnimation.play();
+
+        ComboBox<String> roleDropdown = createRoleDropdown();
+        roleDropdown.setStyle("-fx-font-size: 16px; -fx-background-color: #f0f0f0; -fx-border-color: #999999;-fx-background-radius: 20");
+        BorderPane.setAlignment(roleDropdown, Pos.TOP_RIGHT);
+        BorderPane.setMargin(roleDropdown, new Insets(-30, 10, 0, 0));
 
         Button logoutButton = createLogoutButton();
+        BorderPane.setAlignment(logoutButton, Pos.BOTTOM_RIGHT);
+        BorderPane.setMargin(logoutButton, new Insets(100, 10, 10, 100));
 
-        layout.getChildren().addAll(greetingLabel, createRoleDropdown(), logoutButton);
+        layout.setTop(greetingLabel);
+        layout.setRight(roleDropdown);
+        layout.setBottom(logoutButton);
 
         return new Scene(layout, SCREEN_RES_WIDTH, SCREEN_RES_HEIGHT);
     }
     public Scene createScene(boolean toggleManager) {
-        VBox layout = new VBox(20);
-        layout.setAlignment(Pos.CENTER);
+        BorderPane layout = new BorderPane();
         layout.setPadding(new Insets(20));
 
         Label greetingLabel = new Label("Welcome General Staff " + employeeName + "!");
         greetingLabel.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
+        BorderPane.setAlignment(greetingLabel, Pos.TOP_LEFT);
+        BorderPane.setMargin(greetingLabel, new Insets(10, 0, 0, 10));
+
+        Timeline labelAnimation = new Timeline(
+                new KeyFrame(Duration.ZERO, new KeyValue(greetingLabel.opacityProperty(), 0)),
+                new KeyFrame(Duration.seconds(1), new KeyValue(greetingLabel.opacityProperty(), 1))
+        );
+        labelAnimation.play();
 
         Button logoutButton = createLogoutButton();
+        BorderPane.setAlignment(logoutButton, Pos.BOTTOM_RIGHT);
+        BorderPane.setMargin(logoutButton, new Insets(100, 10, 10, 100));
+        ComboBox<String> roleDropdown = new ComboBox<>();
+        if (toggleManager){
+            roleDropdown = createRoleDropdownManager();
+            roleDropdown.setStyle("-fx-font-size: 16px; -fx-background-color: #f0f0f0; -fx-border-color: #999999;-fx-background-radius: 20");
+            BorderPane.setAlignment(roleDropdown, Pos.TOP_RIGHT);
+            BorderPane.setMargin(roleDropdown, new Insets(-30, 10, 0, 0));
+        }
+        else {
+            roleDropdown = createRoleDropdownDirector();
+            roleDropdown.setStyle("-fx-font-size: 16px; -fx-background-color: #f0f0f0; -fx-border-color: #999999;-fx-background-radius: 20");
+            BorderPane.setAlignment(roleDropdown, Pos.TOP_RIGHT);
+            BorderPane.setMargin(roleDropdown, new Insets(-30, 10, 0, 0));
+        }
 
-        if (toggleManager)
-            layout.getChildren().addAll(greetingLabel, createRoleDropdownManager(), logoutButton);
-        else
-            layout.getChildren().addAll(greetingLabel, createRoleDropdownDirector(), logoutButton);
+        layout.setTop(greetingLabel);
+        layout.setRight(roleDropdown);
+        layout.setBottom(logoutButton);
 
         return new Scene(layout, SCREEN_RES_WIDTH, SCREEN_RES_HEIGHT);
     }
